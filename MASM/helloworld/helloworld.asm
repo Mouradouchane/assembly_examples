2efdbf0b7ed439c7c666239ef74516dbfd0a3474
Mouradouchane <mouradouchane@gmail.com>
2023-05-03 08:40:26 +0000

    edit:changes helloworld example in windows masm to use getchar

--- MASM/helloworld/helloworld.asm
+++ MASM/helloworld/helloworld.asm
@@ -9,19 +9,17 @@ include \masm32\include\masm32.inc
 
 includelib \masm32\lib\kernel32.lib
 includelib \masm32\lib\masm32.lib
-;includelib \masm32\lib\stdin.lib
+includelib \masm32\lib\stdin.lib
 
 
 .data
     HelloWorld db "Hello World!", 0
-    var1 db "is this gonna work !", 0
     pause_str db "pause" , 0
 
 .code
     start:
 
         invoke StdOut, addr HelloWorld
-        invoke StdOut, addr var1
         
         invoke StdIn , addr pause_str , 1
     
